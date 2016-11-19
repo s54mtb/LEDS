ea05f55b8ce22037b27e4c557b43001bc0b99af9
Marko Pavlin <marko@pavlin.si>
2016-11-20 00:12:12 +0100

    Windows application: Shades editor --- alpha version

---
+++ WINapp/SE/SE/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxShades = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CopyPrevBtn = new System.Windows.Forms.Button();
+            this.PrevBtn = new System.Windows.Forms.Button();
+            this.MarkPrevBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.MarkNextBtn = new System.Windows.Forms.Button();
+            this.CopyNextBtn = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.ClrAllBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxShades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxShades
+            // 
+            this.pictureBoxShades.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBoxShades.Image = global::SE.Properties.Resources.background150inv;
+            this.pictureBoxShades.Location = new System.Drawing.Point(0, 15);
+            this.pictureBoxShades.Name = "pictureBoxShades";
+            this.pictureBoxShades.Size = new System.Drawing.Size(1005, 382);
+            this.pictureBoxShades.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxShades.TabIndex = 0;
+            this.pictureBoxShades.TabStop = false;
+            this.pictureBoxShades.Tag = "-1";
+            this.pictureBoxShades.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxShades_Paint);
+            this.pictureBoxShades.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Pixel_MouseMove_1);
+            this.pictureBoxShades.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pixel_MouseMove_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(483, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pix??";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 557);
+            this.hScrollBar1.Maximum = 1024;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(1005, 16);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.ForeColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(12, 521);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.ForeColor = System.Drawing.Color.LightGray;
+            this.button2.Location = new System.Drawing.Point(93, 521);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CopyPrevBtn
+            // 
+            this.CopyPrevBtn.BackColor = System.Drawing.Color.DimGray;
+            this.CopyPrevBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.CopyPrevBtn.Location = new System.Drawing.Point(351, 521);
+            this.CopyPrevBtn.Name = "CopyPrevBtn";
+            this.CopyPrevBtn.Size = new System.Drawing.Size(75, 23);
+            this.CopyPrevBtn.TabIndex = 7;
+            this.CopyPrevBtn.Text = "< Copy";
+            this.CopyPrevBtn.UseVisualStyleBackColor = false;
+            this.CopyPrevBtn.Click += new System.EventHandler(this.CopyPrevBtn_Click);
+            // 
+            // PrevBtn
+            // 
+            this.PrevBtn.BackColor = System.Drawing.Color.DimGray;
+            this.PrevBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.PrevBtn.Location = new System.Drawing.Point(270, 521);
+            this.PrevBtn.Name = "PrevBtn";
+            this.PrevBtn.Size = new System.Drawing.Size(75, 23);
+            this.PrevBtn.TabIndex = 8;
+            this.PrevBtn.Text = "<<<";
+            this.PrevBtn.UseVisualStyleBackColor = false;
+            this.PrevBtn.Click += new System.EventHandler(this.PrevBtn_Click);
+            // 
+            // MarkPrevBtn
+            // 
+            this.MarkPrevBtn.BackColor = System.Drawing.Color.DimGray;
+            this.MarkPrevBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.MarkPrevBtn.Location = new System.Drawing.Point(432, 521);
+            this.MarkPrevBtn.Name = "MarkPrevBtn";
+            this.MarkPrevBtn.Size = new System.Drawing.Size(75, 23);
+            this.MarkPrevBtn.TabIndex = 9;
+            this.MarkPrevBtn.Text = "< Mark";
+            this.MarkPrevBtn.UseVisualStyleBackColor = false;
+            this.MarkPrevBtn.Click += new System.EventHandler(this.MarkPrevBtn_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.BackColor = System.Drawing.Color.DimGray;
+            this.NextBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.NextBtn.Location = new System.Drawing.Point(675, 521);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(75, 23);
+            this.NextBtn.TabIndex = 12;
+            this.NextBtn.Text = ">>>";
+            this.NextBtn.UseVisualStyleBackColor = false;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // MarkNextBtn
+            // 
+            this.MarkNextBtn.BackColor = System.Drawing.Color.DimGray;
+            this.MarkNextBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.MarkNextBtn.Location = new System.Drawing.Point(513, 521);
+            this.MarkNextBtn.Name = "MarkNextBtn";
+            this.MarkNextBtn.Size = new System.Drawing.Size(75, 23);
+            this.MarkNextBtn.TabIndex = 11;
+            this.MarkNextBtn.Text = "Mark >";
+            this.MarkNextBtn.UseVisualStyleBackColor = false;
+            this.MarkNextBtn.Click += new System.EventHandler(this.MarkNextBtn_Click);
+            // 
+            // CopyNextBtn
+            // 
+            this.CopyNextBtn.BackColor = System.Drawing.Color.DimGray;
+            this.CopyNextBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.CopyNextBtn.Location = new System.Drawing.Point(594, 521);
+            this.CopyNextBtn.Name = "CopyNextBtn";
+            this.CopyNextBtn.Size = new System.Drawing.Size(75, 23);
+            this.CopyNextBtn.TabIndex = 10;
+            this.CopyNextBtn.Text = "Copy >";
+            this.CopyNextBtn.UseVisualStyleBackColor = false;
+            this.CopyNextBtn.Click += new System.EventHandler(this.CopyNextBtn_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.DimGray;
+            this.button9.ForeColor = System.Drawing.Color.LightGray;
+            this.button9.Location = new System.Drawing.Point(913, 521);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Upload";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // ClrAllBtn
+            // 
+            this.ClrAllBtn.BackColor = System.Drawing.Color.DimGray;
+            this.ClrAllBtn.ForeColor = System.Drawing.Color.LightGray;
+            this.ClrAllBtn.Location = new System.Drawing.Point(172, 521);
+            this.ClrAllBtn.Name = "ClrAllBtn";
+            this.ClrAllBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClrAllBtn.TabIndex = 14;
+            this.ClrAllBtn.Text = "CLR All";
+            this.ClrAllBtn.UseVisualStyleBackColor = false;
+            this.ClrAllBtn.Click += new System.EventHandler(this.ClrAllBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(483, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "#0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1005, 573);
+            this.Controls.Add(this.ClrAllBtn);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.MarkNextBtn);
+            this.Controls.Add(this.CopyNextBtn);
+            this.Controls.Add(this.MarkPrevBtn);
+            this.Controls.Add(this.PrevBtn);
+            this.Controls.Add(this.CopyPrevBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxShades);
+            this.MaximumSize = new System.Drawing.Size(1013, 600);
+            this.MinimumSize = new System.Drawing.Size(1013, 600);
+            this.Name = "Form1";
+            this.Text = "LED Shades Editor";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.LocationChanged += new System.EventHandler(this.Form1_Activated);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxShades_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxShades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxShades;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button CopyPrevBtn;
+        private System.Windows.Forms.Button PrevBtn;
+        private System.Windows.Forms.Button MarkPrevBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button MarkNextBtn;
+        private System.Windows.Forms.Button CopyNextBtn;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button ClrAllBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WINapp/SE/SE/Form1.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SE
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        PictureBox[] PixelArray = new PictureBox[160];
+        Label[] ByteLabels1 = new Label[20];
+        uint[] PixelActive = new uint[160];
+        uint[] PixelMarked = new uint[160];
+        uint[] PixelOn = new uint[160];
+        byte[] imgBytes = new byte[20*1024];
+        int CurrentFrame = 0;
+
+        int lastnum = -1;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.SetStyle(
+              ControlStyles.AllPaintingInWmPaint |
+              ControlStyles.UserPaint |
+              ControlStyles.DoubleBuffer, true);
+
+            hScrollBar1.Maximum = imgBytes.Length / 20 - 1;
+            // Init and Draw "Pixels" ... 
+            InitPixels();
+            DrawGrid();
+
+            // Add byte labels
+            int horiz = 17;
+            for (int i=0; i<20; i++)
+            {
+                ByteLabels1[i] = new Label();
+                ByteLabels1[i].Location = new Point(horiz, 2);
+                ByteLabels1[i].Text = "0";
+                ByteLabels1[i].ForeColor = Color.LightGray;
+                ByteLabels1[i].Size = new Size(42, 15);
+                ByteLabels1[i].AutoSize = false;
+                ByteLabels1[i].TextAlign = ContentAlignment.MiddleCenter;
+                this.Controls.Add(ByteLabels1[i]);
+                
+                if (i == 9) horiz += 44 * 2;  // Jump over mid section (from left to right)
+                horiz += 44;
+                
+            }
+
+            for (int i=0; i< imgBytes.Length; i++) imgBytes[i] = 0;
+
+            pictureBoxShades.SendToBack();  
+
+        }
+
+        private bool IsPixelOnShades(int idx)
+        {
+            int[] InactivePixel = { 0,1,5,6,7,8,15,73,64,72,79,80,81,87,88,144,151,152,153,157,158,159 };
+
+            return !InactivePixel.Contains(idx);
+        }
+
+        private void UpdateValues()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                byte x = 0;
+                byte c = 128;
+                for (int b=0; b<8; b++)
+                {
+                    if (PixelOn[i * 8 + b] == 1) x += c;
+                    c >>= 1;
+                }
+                imgBytes[i+20* CurrentFrame] = x;
+                ByteLabels1[i].Text = x.ToString();
+            }
+        }
+
+        private void UpdatePixles()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                byte x = imgBytes[i + 20 * CurrentFrame];
+                byte c = 128;
+                for (int b = 0; b < 8; b++)
+                {
+                    if ((x & c) > 0)
+                    {
+                        PixelOn[i * 8 + b] = 1;
+                        DrawPixel(pictureBoxShades, i * 8 + b, Pens.Yellow);
+                    }
+                    else
+                    {
+                        DrawPixel(pictureBoxShades, i * 8 + b, Pens.Black);
+                    }
+
+                    //SetPixelColor(i*8+b);
+                    c >>= 1;
+                }
+                ByteLabels1[i].Text = x.ToString();
+                
+            }
+        }
+
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            HScrollBar hb = (HScrollBar)sender;
+            CurrentFrame = hb.Value;
+
+            for (int i = 0; i < 160; i++)
+            {
+                PixelOn[i] = 0;
+                PixelMarked[i] = 0;
+                MarkPixel(pictureBoxShades, i, Pens.Black);
+            }
+            UpdatePixles();
+            label2.Text = "#"+CurrentFrame.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog 
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Shades Editor file|*.se|Text file|*.txt|C source code|*.c";
+            saveFileDialog1.Title = "Save your LED Shades animation";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+               
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:  // Save as Shades Editor file
+                        File.WriteAllBytes(saveFileDialog1.FileName, imgBytes);
+                        break;
+
+                    case 2: // Save as text
+                        File.WriteAllText(saveFileDialog1.FileName, ByteArrayToString(imgBytes));
+
+                        break;
+
+                    case 3: // Save as C source
+                        
+
+                        break;
+                }
+
+                
+            }
+
+        }
+
+
+        private static string ByteArrayToString(byte[] byteArray)
+        {
+            var hex = new StringBuilder(byteArray.Length * 2);
+            foreach (var b in byteArray)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        private void PrevBtn_Click(object sender, EventArgs e)
+        {
+            if (hScrollBar1.Value > 0) hScrollBar1.Value--;
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            if (hScrollBar1.Value < hScrollBar1.Maximum) hScrollBar1.Value++;
+        }
+
+        private void MarkFrom(int WhichFrame)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                byte x = imgBytes[i + 20 * WhichFrame];
+                byte c = 128;
+                for (int b = 0; b < 8; b++)
+                {
+                    if ((x & c) > 0)
+                    {
+                        PixelMarked[i * 8 + b] = 1;
+                        MarkPixel(pictureBoxShades, i * 8 + b, Pens.Beige);
+                    }
+                    else
+                    {
+                        PixelMarked[i * 8 + b] = 0;
+                        MarkPixel(pictureBoxShades, i * 8 + b, Pens.Black);
+                    }
+                    //SetPixelColor(i * 8 + b);
+                    c >>= 1;
+                }
+                
+            }
+        }
+
+        private void CopyFrom(int WhichFrame)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                imgBytes[i + 20 * CurrentFrame] = imgBytes[i + 20 * WhichFrame];
+                UpdatePixles();
+            }
+        }
+
+
+        private void MarkPrevBtn_Click(object sender, EventArgs e)
+        {
+            if (CurrentFrame > 0)
+            {
+                MarkFrom(CurrentFrame - 1);
+            }
+        }
+
+        private void MarkNextBtn_Click(object sender, EventArgs e)
+        {
+            if (CurrentFrame < 1024)
+            {
+                MarkFrom(CurrentFrame + 1);
+            }
+        }
+
+        private void CopyPrevBtn_Click(object sender, EventArgs e)
+        {
+            if (CurrentFrame > 0)
+            {
+                CopyFrom(CurrentFrame - 1);
+            }
+        }
+
+        private void CopyNextBtn_Click(object sender, EventArgs e)
+        {
+            if (CurrentFrame < 1024)
+            {
+                CopyFrom(CurrentFrame + 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog 
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Shades Editor file|*.se";
+            openFileDialog1.Title = "Open LED Shades animation";
+            openFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string ....
+            if (openFileDialog1.FileName != "")
+            {
+                imgBytes = File.ReadAllBytes(openFileDialog1.FileName);
+                hScrollBar1.Value = 0;
+                CurrentFrame = 0;
+                UpdatePixles();
+            }
+        }
+
+        private void ClrAllBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Sure", "Clear All ?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                for (int i = 0; i < imgBytes.Length; i++) imgBytes[i] = 0;
+                for (int i = 0; i < PixelArray.Length; i++)
+                {
+                    PixelMarked[i] = 0;
+                    PixelOn[i] = 0;
+                }
+                CurrentFrame = 0;
+                hScrollBar1.Value = 0;
+            }
+            
+            UpdatePixles();
+        }
+
+        private int coord2num(int x, int y)
+        {
+            int xn = -1, yn = -1, n;
+            if (x > (22 + 44 * 10)) x -= 2 * 44;   // skip mid section
+            if ((x > 22) & (y > 17))
+            {
+                xn = (x - 22) / 44;
+                yn = (y - 17) / 35;
+            }
+
+            if ((xn < 20) & (xn >= 0) & (yn < 8) & (yn >= 0))
+            {
+                n = xn * 8 + yn;
+            }
+            else
+            {
+                n = -1;
+            }
+
+            return n;
+        }
+
+        private void DrawMarkRect( object sender, int num, Pen p)
+        {
+            int xn = num / 8;
+            int yn = num % 8;
+            int x, y;
+
+            PictureBox box = (PictureBox)sender;
+            Graphics gF = box.CreateGraphics();
+
+            x = 22 + xn * 44;
+            y = 17 + yn * 35;
+            if (num >= 79) x += 2 * 44;
+            if (IsPixelOnShades(num)) gF.DrawRectangle(p, x, y, 41, 33);
+
+        }
+
+        private void DrawGrid()
+        {
+            for (int i = 0; i < 160; i++)
+            {
+                DrawMarkRect(pictureBoxShades, i, Pens.Gray);
+            }
+
+        }
+
+        private void DrawPixel(object sender, int num, Pen p)
+        {
+            int xn = num / 8;
+            int yn = num % 8;
+            int x, y;
+
+            PictureBox box = (PictureBox)sender;
+            Graphics gF = box.CreateGraphics();
+
+            x = 24 + xn * 44;
+            y = 19 + yn * 35;
+            if (num >= 79) x += 2 * 44;
+
+            SolidBrush b = new SolidBrush(p.Color);
+            if (IsPixelOnShades(num)) gF.FillRectangle(b, x, y, 38, 30);
+
+        }
+
+
+        private void MarkPixel(object sender, int num, Pen p)
+        {
+            int xn = num / 8;
+            int yn = num % 8;
+            int x, y;
+
+            PictureBox box = (PictureBox)sender;
+            Graphics gF = box.CreateGraphics();
+
+            x = 23 + xn * 44;
+            y = 18 + yn * 35;
+            if (num >= 79) x += 2 * 44;
+
+            if (IsPixelOnShades(num)) gF.DrawRectangle(p, x, y, 39, 31);
+
+        }
+
+
+        void InitPixels()
+        {
+            for (int i = 0; i < PixelArray.Length; i++)
+            {
+                PixelActive[i] = 0;
+                PixelMarked[i] = 0;
+                PixelOn[i] = 0;
+            }
+
+        }
+
+        private void Pixel_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            PictureBox pixel = (PictureBox)sender;
+            int num;                     
+
+            if (pixel != null)
+            {
+                num = coord2num(e.X, e.Y);
+                label1.Text = "Pix#" + num;
+                if (num!=lastnum)
+                {
+                    DrawGrid();
+                    if (num >= 0) DrawMarkRect(sender, num, Pens.White);
+                    lastnum = num;
+                }
+
+                if ((e.Button == MouseButtons.Left) & (num >= 0))
+                {
+                    PixelOn[num] = 1;
+                    DrawPixel(sender, num, Pens.Yellow);
+                }
+
+                if ((e.Button == MouseButtons.Right) & (num >= 0))
+                {
+                    PixelOn[num] = 0;
+                    DrawPixel(sender, num, Pens.Black);
+                }
+                UpdateValues();
+            }
+        }
+
+        private void pictureBoxShades_Refresh()
+        {
+            DrawGrid();
+            for (int i=0; i<160; i++)
+            {
+                if (PixelMarked[i] == 1)
+                {
+                    MarkPixel(pictureBoxShades, i, Pens.Beige);
+                }
+                else
+                {
+                    MarkPixel(pictureBoxShades, i, Pens.Black);
+                }
+                if (PixelOn[i] == 1)
+                {
+                    DrawPixel(pictureBoxShades, i, Pens.Yellow);
+                }
+                else
+                {
+                    DrawPixel(pictureBoxShades, i, Pens.Black);
+                }
+            }
+        }
+
+        private void pictureBoxShades_Paint(object sender, PaintEventArgs e)
+        {
+            // Refresh graphics
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            // Refresh graphics
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Refresh graphics
+            pictureBoxShades_Refresh();
+            timer1.Enabled = false;
+        }
+    }
+}
